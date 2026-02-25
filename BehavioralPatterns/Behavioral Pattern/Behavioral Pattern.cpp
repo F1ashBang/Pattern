5b746f1237731fd419bd3c5073b51b967256fc7b
Patterns <>
2026-02-25 19:42:58 +0500

    Обновил примеры

--- BehavioralPatterns/Behavioral Pattern/Behavioral Pattern.cpp
+++ BehavioralPatterns/Behavioral Pattern/Behavioral Pattern.cpp
@@ -7,9 +7,10 @@ using namespace std;
 class ComputerSystem
 {
 public:
-    void openFile(const string& file) { cout << "Открываем файл: " << file << endl; }
+    void openFile(const string& path) { cout << "Открываем файл по пути: " << path << endl; }
     void reload() { cout << "Перезагружаем компьютер" << endl; }
     void launchBrowser() { cout << "Запускаем Браузер" << endl; }
+    void launchPaint() { cout << "Запускаем пейнт" << endl; }
 };
 
 class Command
@@ -23,11 +24,11 @@ class OpenFileCommand : public Command
 {
 private:
     ComputerSystem* system;
-    string fileName;
+    string path;
 public:
-    OpenFileCommand(ComputerSystem* s, const string& fn) : system(s), fileName(fn) {}
-    void execute() override { system->openFile(fileName); }
-    void undo() override { cout << "Закрываем файл: " << fileName << endl; }
+    OpenFileCommand(ComputerSystem* s, const string& fn) : system(s), path(fn) {}
+    void execute() override { system->openFile(path); }
+    void undo() override { cout << "Закрываем файл по пути: " << path << endl; }
 };
 
 class ReloadCommand : public Command
@@ -51,6 +52,18 @@ public:
     void execute() override { system->launchBrowser(); }
     void undo() override { cout << "Закрываем браузер" << endl; }
 };
+
+class LaunchPaintCommand : public Command
+{
+private:
+    ComputerSystem* system;
+    string fileName;
+public:
+    LaunchPaintCommand(ComputerSystem* s) : system(s) {}
+    void execute() override { system->launchPaint(); }
+    void undo() override { cout << "Закрываем пейнт" << endl; }
+};
+
 
 class RemoteControl
 {
@@ -80,13 +93,15 @@ int main()
     setlocale(0, "");
     ComputerSystem system;
 
-    Command* openCmd = new OpenFileCommand(&system, "code.cpp");
+    Command* openCmd = new OpenFileCommand(&system, "C:/User/source/repos/code.cpp");
     Command* reloadCmd = new ReloadCommand(&system);
     Command* browserCmd = new LaunchBrowserCommand(&system);
+    Command* PaintCmd = new LaunchPaintCommand(&system);
 
     RemoteControl remote;
     remote.addCommand(openCmd);
     remote.addCommand(browserCmd);
+    remote.addCommand(PaintCmd);
     remote.addCommand(reloadCmd);
 
     remote.pressButton();
