019ff9cb50bd1435222bbadc565cce27368012eb
Patterns <>
2026-02-25 19:26:34 +0500

    Обновил пример Порождающего паттерна

--- CreationalPatterns/Creational Pattern/Creational Pattern.cpp
+++ CreationalPatterns/Creational Pattern/Creational Pattern.cpp
@@ -8,6 +8,7 @@ class Character
 public:
     virtual string getType() = 0;
     virtual void attak() = 0;
+    virtual int getHealth() = 0;
 };
 
 class Warrior : public Character
@@ -15,6 +16,15 @@ class Warrior : public Character
 public:
     string getType() override { return "Воин"; }
     void attak() override { cout << "Воин аттакует мечом!" << endl; }
+    int getHealth() override { return 100; }
+};
+
+class Knight : public Character
+{
+public:
+    string getType() override { return "Рыцарь"; }
+    void attak() override { cout << "Рыцарь атакует копьём!" << endl; }
+    int getHealth() override { return 80; }
 };
 
 class Mage : public Character
@@ -22,6 +32,7 @@ class Mage : public Character
 public:
     string getType() override { return "Маг"; }
     void attak() override { cout << "Маг кастует заклинание!" << endl; }
+    int getHealth() override { return 50; }
 };
 
 class Archer : public Character
@@ -29,6 +40,7 @@ class Archer : public Character
 public:
     string getType() override { return "Лучник"; }
     void attak() override { cout << "Лучник стреляет из лука!" << endl; }
+    int getHealth() override { return 70; }
 };
 
 class CharacterFactory 
@@ -38,7 +50,7 @@ public:
     void spawnAndAttak()
     {
         Character* charact = createHero();
-        cout << "Создан " << charact->getType() << ". ";
+        cout << "Создан " << charact->getType() << " со здоровьем " << charact->getHealth() << ". ";
         charact->attak();
         delete charact;
     }
@@ -48,6 +60,11 @@ class WarriorFactory : public CharacterFactory
 {
 public:
     Character* createHero() override { return new Warrior(); }
+};
+class KnightFactory : public CharacterFactory
+{
+public:
+    Character* createHero() override { return new Knight(); }
 };
 
 class MageFactory : public CharacterFactory
@@ -73,5 +90,8 @@ int main()
     factory->spawnAndAttak();
 
     factory = new ArcherFactory();
+    factory->spawnAndAttak();
+
+    factory = new KnightFactory();
     factory->spawnAndAttak();
 }
